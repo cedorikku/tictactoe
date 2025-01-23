18c331e702fa97d0e3d5d2de3a72834372a2bc27
Cedric <75742716+cedorikku@users.noreply.github.com>
2025-01-23 19:40:46 +0800

    Add game logic

--- script.js
+++ script.js
@@ -16,10 +16,11 @@ const gameboard = (function() {
     };
 
     const drawShape = (row, col, shape) => {
-        if (board[row][col] === '') return;
+        if (board[row][col].getValue() !== '') return false;
 
         board[row][col].addShape(shape);
         console.log(`An ${shape} has been placed.`);
+        return true;
     }
 
     function Cell() {
@@ -42,3 +43,55 @@ const gameboard = (function() {
         drawShape
     };
 })();
+
+const gameController = (function(
+    board,
+    playerOneName = "Player 1",
+    playerTwoName = "Player 2",
+) {
+
+    const players = [
+        {
+            name: playerOneName,
+            shape: 'O'
+        },
+        {
+            name: playerTwoName,
+            shape: 'X',
+        },
+    ]
+
+    let activePlayer = players[0];
+    const getActivePlayer = () => activePlayer;
+
+    const switchActiveTurn = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    }
+
+    const printNewRound = () => {
+        board.printBoard();   
+        console.log(`It's now ${getActivePlayer().name}'s turn`)
+    };
+
+    const playRound = (row, column) => {
+        if (!board.drawShape(row, column, getActivePlayer().shape)) {
+            console.log("Invalid move");
+            printNewRound();
+        }
+
+        switchActiveTurn();
+        printNewRound();
+    };
+    
+    printNewRound();
+
+    return {
+        getActivePlayer,
+        playRound,
+    };
+
+})(gameboard);
+
+gameController.playRound(1, 1);
+gameController.playRound(0, 1);
+gameController.playRound(0, 1);
